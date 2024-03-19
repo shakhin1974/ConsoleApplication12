fc717db0c733fcfa8c06bce19d6df704cb13a94d
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-03-19 15:23:40 +0300

    ConsoleApplication12.cpp

--- ConsoleApplication12/ConsoleApplication12.cpp
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -1,9 +1,15 @@
-﻿#include <iostream>
+#include <iostream>
 #include<string>
 #include"Avto.h"
 #include<windows.h>
 #include<iomanip>
 using namespace std;
+
+void stop()
+{
+
+}
+
 int main()
 {
  SetConsoleCP(1251);
@@ -12,15 +18,24 @@ int main()
         BMW("BMW", "316", 2014, 1.9, 220, 2500),
         Mersedez("Mersedez", "C45", 2008, 2.1, 230, 3200),
         MersedezCE45("Mersedez", "CE5", 2011, 2.3, 225, 3900);
-    cout << "Время нажатия на педаль";
+    cout << "Время нажатия на педаль Газ";
     int n;  
     cin >> n;
     cout << "Укаажи ускарение скорости за одну секунду";
-    int speed;
-    cin >> speed;
+    
+    
     for (int  i = 0; i < n; i++)
     {
-        cout << BMW.skorost(speed)<<endl;
-        speed = BMW.skorost(speed)+6;
+        cout << BMW.GAZ(BMW.speed)<<endl;
+        BMW.speed = BMW.GAZ(BMW.speed)+6;
     }
+    cout << "Введите секунды для тормоза: ";
+    int time;
+    cin >> time;
+    cout << endl;
+
+    BMW.stop(time);
+
 }
+
+
