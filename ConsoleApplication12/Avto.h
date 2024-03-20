889edaec14e1d71edea4267c7bb27522a1c55c2d
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-03-20 11:03:16 +0300

    Avto.h

--- ConsoleApplication12/Avto.h
+++ ConsoleApplication12/Avto.h
@@ -3,65 +3,46 @@
 #include <string>
 using namespace std;
 class Avtomobil
-{
-public:
+{ public:
 	int speed = 0;
-	Avtomobil(string b, string m,
-		int y, float o, int MS, int w)
-	{
-		brand = b;
-		marka = m;
-		year = y;
-		obyem = o;
-		MaxSpeed = MS;
-		weigth = w;
-		
-
-	}
+	Avtomobil(string b, string m, int y, float o, int MS, int w)
+	{ brand = b;
+	marka = m;
+	year = y;
+	obyem = o;
+	MaxSpeed = MS;
+	weigth = w; }
 	 ~Avtomobil() {}
-void util()
-{
-	if (year < 2004)
-	{
-		cout << "Автомобиль " << brand << " " << marka
-			<< " подлежащит к  утилизации \n";
-	}
-	else 
-	{
-		cout << "Автомобиль " << brand << " " << marka
-			<< " НЕ подлежащит к  утилизации \n";
-	}
-}
-int GAZ( int Speed )
-{
-	int q = 0;
-	if (Speed < MaxSpeed - 40) {
-		return q + Speed;
-	}
-}
-
-int stop (int time)
+int GAZ( int time )
+{ for (int i = 0; i < time; i++)
+	{ if (speed <= MaxSpeed  )
+	{ speed = speed + 8;
+	cout << speed<<endl;} 
+} 	return  speed;  }
+int FREE (int time)
 {
 	for (int i = 0; i < time; i++)
 	{
-		if (speed > 0 ) {
-			speed = speed  / 1.5;
+		if (speed <= 10)
+		{
+			speed = speed -8;
 			cout << speed << endl;
-			
 		}
-		
-	}
-	 return speed;
-	
-	
+	} 	return  speed;
 }
-
+int STOP (int time)
+{ 	for (int i = 0; i < time; i++)
+	{ if (speed >= 15 ) 
+	{speed = speed - 14;
+	cout << speed << endl; }
+	if (speed < 10)
+	{ speed = 0;
+	cout << speed << endl; 	break; } }
+return speed ; }
 private:
 	string brand{};
 	string marka{};
 	int year{};
 	float obyem{};
 	int MaxSpeed{};
-	int weigth;
-	
-};
+	int weigth; };
