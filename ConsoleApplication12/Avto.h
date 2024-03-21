f97495cd62f9785da48473bda342f6cb627ea34e
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-03-21 13:23:16 +0300

    Avto.h

--- ConsoleApplication12/Avto.h
+++ ConsoleApplication12/Avto.h
@@ -4,59 +4,92 @@
 using namespace std;
 class Avtomobil
 { public:
-	int speed = 0;
-	int way = 0;
-	Avtomobil(string b, string m, int y, double o, int MS, int w)
-	{ 
-	brand = b;
-	marka = m;
-	year = y;
-	obyem = o;
-	MaxSpeed = MS;
-	weigth = w;
- 
+	double speed = 0.0;
+	double way = 0.0;
+	double k = 0.0;
+	Avtomobil(string b, string m, int y,
+		double d, double MS, int w, int buck,
+		double t = 0.0)
+	{
+		brand = b;
+		marka = m;
+		year = y;
+		dvigatel = d;
+		MaxSpeed = MS;
+		weigth = w;
+		toplivo = t;
 	}
 	 ~Avtomobil() {}
-int GAZ( int time )
-{ for (int i = 0; i < time; i++)
-
-	{ if (speed <= MaxSpeed   )
-	 speed = speed + 8;
-	way += time *speed / 3600;
-	cout << "Speed " << speed << " Way "<<way<<endl; 
-} 	return  speed, way;  }
-int FREE (int time)
+	 double GAZ(int time)
+	 {	k = k+ time;
+	 for (int i = 0; i < time; i++)
+		 {
+			 if (speed <= MaxSpeed - 40.00)
+				 if (toplivo < bak)
+					 break;
+				 else
+			
+speed = round(speed + 8.00);
+way += (speed) / 3600.00;
+toplivo -= way / 9000;
+cout << "Gaz i =" << round(i/60.00) << " Speed " << speed
+<< " Way " << way << " toplivo " 
+<< toplivo << endl;
+if (toplivo < 0)
+break;
+} 	 cout << "Время на дороге = " << k << endl;
+	 return  speed, way, toplivo;
+	
+}
+double FREE (int time)
 {
+	k = k+ time;
 	for (int i = 0; i < time; i++)
-	{
-		if (speed <= 10)
-		{
-			speed = speed -8;
-			way += time * speed / 3600;			
-			cout << "Speed " << speed << " Way " << way << endl;
-		}
-	} 	return  speed, way;
+	{ if (speed >= 10.00)
+	speed = round (speed -8.00);
+	way += (speed) / 3600.00;
+	toplivo -= way / 9900;
+	cout << "Free i =" << round (i / 60.00) 
+		<< " Speed " << speed
+		<< " Way " << way << " toplivo "
+		<< toplivo << endl;
+	if (toplivo < 0)
+		break;	
 
+	} 	
+	cout << "Время на дороге = " << k << endl;
+	return  speed, way, toplivo;
 }
-int STOP (int time)
-{ 	for (int i = 0; i < time; i++)
-	{ if (speed >= 5 ) 
+double STOP (int time)
+{
+	k = k + time;
+	for (int i = 0; i < time; i++)
+	{ if (speed >= 5.00 ) 
 	{
-	cout <<"Speed " << speed <<" Way "<<way<< endl;
-	speed -= 24;
-	way += time * speed / 3600;
+	speed -= 18.00;
+	way += (speed) / 360.00;
+	toplivo -= way / 9000;
+cout << "Stop i =" << round (i / 60.00) << " Speed " << speed
+<< " Way " << way << " toplivo " << toplivo << endl;
+	if (toplivo < 0)
+		break;
 }
-	if (speed < 1)
+	if (speed < 5.00)
 	{ speed = 0;
 	cout << speed << endl; 	break; } }
-return speed , way; }
+	cout << "Время на дороге = " << k << endl;
+return  speed, way, toplivo;
+}
+void vivod() {
+	cout << "Общее время на дороге = " << k / 60<<endl;
+}
 private:
 	string brand{};
 	string marka{};
 	int year{};
-	double obyem{};
-	int MaxSpeed{};
+	double dvigatel{};
+	double MaxSpeed{};
 	int weigth{};
-	
- 
+	int bak{};
+	double toplivo {}; 
 };
