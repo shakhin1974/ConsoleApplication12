45a62ca80177a103031853af01332cd45f77162f
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-03-21 13:23:52 +0300

    ConsoleApplication12.cpp

--- ConsoleApplication12/ConsoleApplication12.cpp
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -9,20 +9,35 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+   
+    cout << "Сколько литров залить в бак \n";  
+    int topl;
+    cin >> topl;
     Avtomobil
-        BMW("BMW", "316", 2014, 1.9, 220, 2500 ),
-        Mersedez("Mersedez", "C45", 2008, 2.1, 230, 3200),
-        MersedezCE45("Mersedez", "CE5", 2011, 2.3, 225, 3900);
-    cout << "Время нажатия на педаль Газ: ";
-    int n;
-    cin >> n;
-    cout << "Укаажи ускарение скорости за одну секунду";
-    BMW.GAZ(n);
-    cout << "Введите секунды для тормоза: ";
-    int time;
-    cin >> time;
-    cout << endl;
-    BMW.STOP(time);
-    
+        
+BMW("BMW", "316", 2014, 1.9, 220.00, 
+2500, 40, topl ),
+Mersedez("Mersedez", "C45", 2008, 
+2.1, 230.00, 55, topl),
+MersedezCE45("Mersedez", "CE5", 2011, 
+2.3, 225.00, 60, topl);
+
+BMW.GAZ(1200);
+BMW.STOP(5);
+BMW.FREE(8);
+BMW.GAZ(120);
+BMW.GAZ(70);
+BMW.STOP(8);
+BMW.FREE(18);
+BMW.GAZ(120);
+BMW.GAZ(120);
+BMW.STOP(5);
+BMW.FREE(8);
+BMW.GAZ(120);
+BMW.GAZ(70);
+BMW.STOP(8);
+BMW.FREE(18);
+BMW.GAZ(120);
+BMW.vivod();
 }
 
